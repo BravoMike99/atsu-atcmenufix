4accad29debd7100809d1cc7ec074c1b3038d097
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2023-10-30 20:26:06 +0000

    feat(irs monitor): show TTN and use maint word
    feat(irs monitor): add page refresh

--- fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_IRSMonitor.js
+++ fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_IRSMonitor.js
@@ -30,17 +30,36 @@ class CDUIRSMonitor {
         mcdu.onLeftInput[2] = () => {
             CDUIRSStatus.ShowPage(mcdu, 3);
         };
+
+        // regular update due to possible irs status change
+        mcdu.page.SelfPtr = setTimeout(() => {
+            if (mcdu.page.Current === mcdu.page.IRSMonitor) {
+                CDUIRSMonitor.ShowPage(mcdu);
+            }
+        }, mcdu.PageTimeout.Default);
     }
 
     static getAdiruStateMessage(number) {
-        const state = SimVar.GetSimVarValue(`L:A32NX_ADIRS_ADIRU_${number}_STATE`, "Enum");
-        switch (state) {
-            case 1:
-                return "ALIGN";
-            case 2:
-                return "NAV";
-            default:
-                return "";
+        const irMaint = Arinc429Word.fromSimVarValue(`L:A32NX_ADIRS_IR_${number}_MAINT_WORD`);
+        let state = "INVAL";
+        if(irMaint.isNormalOperation()) {
+            // check for alignment if any of the 3 bits are active
+            const bit15 = irMaint.getBitValue(16);
+            const bit16 = irMaint.getBitValue(17);
+            const bit17 = irMaint.getBitValue(18);
+            if(bit15 || bit16 || bit17) {
+                // Time to align is encoded as 3 bits of bit 15,16 and 17
+                const numberBit15 = bit15 ? 1 : 0;
+                const numberBit16 = bit16 ? 2 : 0;
+                const numberBit17 = bit17 ? 4 : 0;
+                state = `ALIGN TTN ${(numberBit15 + numberBit16 + numberBit17)}`;
+            } else if (irMaint.getBitValue(2)) {
+                state = "ATT";
+            } else if(irMaint.getBitValue(3)) {
+                state = "NAV";
+            }
         }
+        return state;
     }
+
 }
