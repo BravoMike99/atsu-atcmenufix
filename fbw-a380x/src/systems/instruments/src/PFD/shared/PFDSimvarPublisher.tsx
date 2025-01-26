9239c897e0af536ff35ea48e87e494015c5d629f
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2025-01-26 13:13:46 +0000

    fix(380x/pfd): remove blink from ls title. Also use LOC for reminder

--- fbw-a380x/src/systems/instruments/src/PFD/shared/PFDSimvarPublisher.tsx
+++ fbw-a380x/src/systems/instruments/src/PFD/shared/PFDSimvarPublisher.tsx
@@ -168,6 +168,7 @@ export interface PFDSimvars {
   fcuLeftVelocityVectorOn: boolean;
   fcuRightVelocityVectorOn: boolean;
   fcuApproachModeActive: boolean;
+  fcuLocModeActive : boolean;
 }
 
 export enum PFDVars {
@@ -336,6 +337,7 @@ export enum PFDVars {
   fcuLeftVelocityVectorOn = 'L:A380X_EFIS_L_VV_BUTTON_IS_ON',
   fcuRightVelocityVectorOn = 'L:A380X_EFIS_R_VV_BUTTON_IS_ON',
   fcuApproachModeActive = 'L:A32NX_FCU_APPR_MODE_ACTIVE',
+  fcuLocModeActive = 'L:A32NX_FCU_LOC_MODE_ACTIVE',
 }
 
 /** A publisher to poll and publish nav/com simvars. */
@@ -503,6 +505,7 @@ export class PFDSimvarPublisher extends UpdatableSimVarPublisher<PFDSimvars> {
     ['fcuLeftVelocityVectorOn', { name: PFDVars.fcuLeftVelocityVectorOn, type: SimVarValueType.Bool }],
     ['fcuRightVelocityVectorOn', { name: PFDVars.fcuRightVelocityVectorOn, type: SimVarValueType.Bool }],
     ['fcuApproachModeActive', { name: PFDVars.fcuApproachModeActive, type: SimVarValueType.Bool }],
+    ['fcuLocModeActive', {name: PFDVars.fcuLocModeActive, type: SimVarValueType.Bool} ]
   ]);
 
   public constructor(bus: EventBus) {
